4c4b6af80855d0dda9f35304d2cfefd6f6162929
7oCode <stu7odent@gmail.com>
2023-01-19 15:52:56 +0800

    Changes made 19th Jan

---
+++ test.py
@@ -0,0 +1,38 @@
+import shelve
+import User
+
+users_dict = {}
+db = shelve.open('user.db', 'r')
+users_dict = db['Users']
+
+users_list = []
+# for key in users_dict:
+#     user = users_dict.get(key)
+#     users_list.append(user)
+#
+
+elist = []
+def afunction():
+    for key in db:
+        user = users_dict.get(key)
+        users_list.append(user)
+        for i in db[key]:
+            # print(i)
+            # print(users_dict[i])
+            elist.append(bfunction(users_dict[i]))
+        bfunction(users_dict[i])
+
+def bfunction(u):
+    return u.get_email_address()
+
+afunction()
+
+
+print(elist)
+if 'Sample@gmail.com' in elist:
+    print('yes')
+else:
+    print('email is incorrect')
+
+
+
